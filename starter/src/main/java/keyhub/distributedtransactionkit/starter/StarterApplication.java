55e33cb2a26e8b2f88f51875f04b085e475ba17f
SUNGHYUK <37289223+Iol-lshh@users.noreply.github.com>
2025-01-22 17:45:38 +0900

    Feature/remote transaction (#7)
    * Refactor project structure and enhance transaction management
    
    - Renamed 'stater' to 'starter' and updated related configurations in `settings.gradle` and `.idea/gradle.xml`.
    - Removed unused exception imports in `AbstractTransactionContext.java`.
    - Improved null checks in `AbstractTransaction.java` for compensation and outbox storage.
    - Introduced new `starter` module with essential Spring Boot setup, including `build.gradle`, application context, and transaction management components.
    - Added `StarterApplication`, `ApplicationContextProvider`, and transaction handling classes to support framework transactions.
    - Implemented event handling for transaction resolution with `AfterTransactionEventHandler`.
    - Created configuration classes for transaction context and thread scope management.
    - Added test cases for transaction functionality and context loading verification in both `starter` and `starter-sandbox` modules.
    - Updated `application.yaml` for database configuration in both modules.
    
    * 리드미 업데이트
    - 보상 사례 추가
    - usecase 업데이트

--- starter/src/main/java/keyhub/distributedtransactionkit/starter/StarterApplication.java
+++ starter/src/main/java/keyhub/distributedtransactionkit/starter/StarterApplication.java
@@ -1,8 +1,10 @@
 package keyhub.distributedtransactionkit.starter;
 
+import keyhub.distributedtransactionkit.starter.component.EnableKhTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableKhTransaction
 @SpringBootApplication
 public class StarterApplication {
     public static void main(String[] args) {
